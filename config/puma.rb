f7a2747beffbd32893aa1e8ca8cd942ea40b296e
Jeremy Ward <jrmy.ward@gmail.com>
2026-01-29 17:48:55 +0000

    Fix: Check SOLID_QUEUE_IN_PUMA as string 'true' not truthy

--- config/puma.rb
+++ config/puma.rb
@@ -35,7 +35,8 @@ port ENV.fetch("PORT", 3000)
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments.
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+# Note: ENV vars are strings, so "false" would be truthy. Check for "true" explicitly.
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] == "true"
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
